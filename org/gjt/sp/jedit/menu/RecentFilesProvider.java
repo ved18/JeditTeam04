71dd6fb0cab24a503df1bac63871ad362dccdbc2
Vedang <vedkapse18@gmail.com>
2023-03-21 19:10:32 -0500

    changed the grep pattern in recentfiles menu to search for string anyhwere in the filename

--- org/gjt/sp/jedit/menu/RecentFilesProvider.java
+++ org/gjt/sp/jedit/menu/RecentFilesProvider.java
@@ -106,7 +106,7 @@ public class RecentFilesProvider implements DynamicMenuProvider
 					if ((! typedText.contains("*")) && (! typedText.contains("?")))
 					{
 						// Old style (before jEdit 4.3pre18): Match start of file name
-						regex = regex + "*";
+						regex = "*" + regex + "*";
 					}
 					pattern = Pattern.compile(StandardUtilities.globToRE(regex),
 						Pattern.CASE_INSENSITIVE);
